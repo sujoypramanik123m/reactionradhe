ef54891e69d9f5866ddcae154bad179e2f39f31e
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-11-30 12:27:48 +0530

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -1,13 +1,35 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
-from nexichat import nexichat
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-@nexichat.on_message(filters.command("start"))
-async def start_message(client: Client, message: Message):
-    await message.reply_text(
-        f"Hello {message.from_user.first_name}! 👋\n\n"
-        "I'm your Reaction Bot! I'll react to every message in groups, channels, and private chats with a 👍 emoji.\n\n"
-        "Add me to your group or channel and watch me in action! 🚀\n\n"
-        "**You can make your bot by /clone😁**"
+
+
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
+
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_photo(
+        chat_id=msg.chat.id,
+        photo="https://files.catbox.moe/y9c134.jpg",
+        caption=f"""❍ ʜᴇʏ  {msg.from_user.mention}  ✤,
+❍ ɪ ᴀᴍ{me2},
+
+❍ Aɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ sᴛʀɪɴɢ sᴇssɪᴏɴ ɢᴇɴᴇʀᴀᴛᴏʀ ʙᴏᴛ, ᴡʀɪᴛᴛᴇɴ ɪɴ ᴩʏᴛʜᴏɴ ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴩ ᴏғ ᴩʏʀᴏɢʀᴀᴍ.
+
+❍ ᴘʟᴇᴀꜱᴇ ᴄʜᴏᴏꜱᴇ ᴛʜᴇ ᴘʏᴛʜᴏɴ ʟɪʙʀᴀʀʏ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ ꜱᴛʀɪɴɢ ꜱᴇꜱꜱɪᴏɴ ꜰᴏʀ.
+
+❍ ɪғ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ, ᴛʜᴇɴ ᴅᴍ ᴛᴏ ᴍʏ ᴏᴡɴᴇʀ: [ᯓ𓆰𝅃 ꯭꯭↬꯭ᬃ꯭ ⃪꯭ ꯭⁢⁣⁤⁣⁣⁢⁣⁤⁢⁤⁣⁢⁤⁣⁤᪳᪳🇷꯭𝚰𝛅꯭꯭ʜ꯭֟፝፝֟ᴜ ꯭꯭༗꯭»꯭݅݅݅݅𓆪 ](t.me/rishu1286) !""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+ 
+                [
+                    InlineKeyboardButton("˹ sᴜᴘᴘᴏʀᴛ ˼", url="https://t.me/rishu1286"),
+                   ], 
+            [
+InlineKeyboardButton("˹ ᴜᴘᴅᴀᴛᴇs ˼", url="https://t.me/rishu1286")
+                ]
+                                
+            ]
+        )
     )
-    
