454a2415ea5f1012c6b7ced426004e6748beb1a4
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-07 13:59:25 +0530

    Update Start.py

--- nexichat/mplugin/Start.py
+++ nexichat/mplugin/Start.py
@@ -1,14 +1,11 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import Message
 from nexichat import nexichat
 
-@nexichat.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
-    # Sending a video along with the reply text
-    await message.reply_video(
-        video="https://envs.sh/RCD.mp4",  # Replace with the actual video URL or local file path
-        caption=(
-            f"""**❖ нᴇʏ  {message.from_user.first_name} !, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
+    await message.reply_text(
+                   f"""**❖ нᴇʏ  {message.from_user.first_name} !, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 ● ɪ ᴀᴍ  {(await client.get_me()).mention} !
@@ -23,12 +20,5 @@ async def start_message(client: Client, message: Message):
 ❍ • ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs •
 ⦿━━━━━━━━━━━━━━━━━━━━━⦿
 
-❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •\n\n❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ /clone**"""
-        ),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("❖ ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ ❖", url="https://t.me/ReactionByBot?startgroup=true")],
-                
-            ]
-        )
+❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •\n\n❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ @ReactionByBot**"""
     )
