12057dee51649b709667c4bc4d63b2865ca71054
live9807 <k76734182@gmail.com>
2025-01-19 12:13:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "580e1a3e7d0719816feebf595c9166d2"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "5738579437"))
+OWNER_ID = int(getenv("OWNER_ID", "6949414014"))
 SUPPORT_GRP = "BOT_SUPPORT_GROUP7"
 UPDATE_CHNL = "ll_BOTCHAMBER_ll"
 OWNER_USERNAME = "RADHE_XD"
