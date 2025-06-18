2a898449a5d0080974545ef5a22d0ecfd9d36a85
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-18 19:13:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ API_ID = "24072444"
 # -------------------------------------------------------------
 API_HASH = "580e1a3e7d0719816feebf595c9166d2"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-MONGO_URL = getenv("MONGO_URL", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_URL = getenv("MONGO_URL", "")
 OWNER_ID = int(getenv("OWNER_ID", "6949414014"))
-SUPPORT_GRP = "BOT_SUPPORT_GROUP7"
-UPDATE_CHNL = "ll_BOTCHAMBER_ll"
-OWNER_USERNAME = "RADHE_XD"
+SUPPORT_GRP = "SuperToppers0"
+UPDATE_CHNL = "SuperToppers"
+OWNER_USERNAME = "UncleChipssBot"
     
