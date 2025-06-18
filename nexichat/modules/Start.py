6ab8ece593880c14b0fc78ac12c688dbda821886
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-19 00:25:32 +0530

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -1,18 +1,13 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from nexichat import nexichat
+from pyrogram.types import Message
 
-@nexichat.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
-    # Sending a video along with the reply text
-    await message.reply_video(
-        video="https://envs.sh/RCD.mp4",  # Replace with the actual video URL or local file path
-        caption=(
-            f"""**❖ нᴇʏ  {message.from_user.first_name} !, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
+    await message.reply_text(
+        f"""**❖ нᴇʏ  {message.from_user.first_name} !, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 ● ɪ ᴀᴍ  {(await client.get_me()).mention} !
-
 
 ⦿━━━━━━━━━━━━━━━━━━━━━⦿
 ❍ •  ɪ'ʟʟ ʀᴇᴀᴄᴛ ᴛᴏ ᴇᴠᴇʀʏ ᴍᴇssᴀɢᴇ  •
@@ -23,12 +18,8 @@ async def start_message(client: Client, message: Message):
 ❍ • ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs •
 ⦿━━━━━━━━━━━━━━━━━━━━━⦿
 
-❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •\n\n❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ /clone**"""
-        ),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton("❖ ᴀᴅᴅ ᴍᴇ ɪɴ ɢʀᴏᴜᴘ ❖", url="https://t.me/REACTION7_ROBOT?startgroup=true")],
-                [InlineKeyboardButton("• sᴜᴘᴘᴏꝛᴛ •", url="https://t.me/ll_BOTCHAMBER_ll")]
-            ]
-        )
+❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •
+
+❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ @REACTION7_ROBOT**""",
+        message_effect_id=5044134455711629726 
     )
