b229b7845d61dcb8b1b0d9a9817b9c5f155c8641
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-20 00:54:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ API_ID = "24072444"
 # -------------------------------------------------------------
 API_HASH = "580e1a3e7d0719816feebf595c9166d2"
 # --------------------------------------------------------------
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-MONGO_URL = getenv("MONGO_URL", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "8027707636:AAHk1jO8S3d6-vaFXVJdtsxVYWHc_U8_-Vw")
+MONGO_URL = getenv("MONGO_URL", "mongodb+srv://sujoy123m:wTWKGUaxYE7dxb1l@cluster0.zorxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(getenv("OWNER_ID", "6949414014"))
 SUPPORT_GRP = "SuperToppers0"
 UPDATE_CHNL = "SuperToppers"
