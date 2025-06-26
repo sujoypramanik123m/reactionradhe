a68d8f7f3572bac71a57ef32ed355f054559b0fe
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-27 00:55:52 +0530

    Update Start.py

--- nexichat/mplugin/Start.py
+++ nexichat/mplugin/Start.py
@@ -1,9 +1,16 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from nexichat import nexichat as app
 
 @Client.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
+    buttons = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/SuperToppers0'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ɢʀᴏᴜᴘ', url='https://t.me/SuperToppers')
+        ]
+    ])
     await message.reply_text(
-                   f"""**❖ Hᴇʏ  {message.from_user.first_name} !, Nɪᴄᴇ Tᴏ Mᴇᴇᴛ Yᴏᴜ !\n\nYᴏᴜ Cᴀɴ Mᴀᴋᴇ Yᴏᴜʀ Oᴡɴ Rᴇᴀᴄᴛɪᴏɴ Bᴏᴛ Bʏ @ReactionCloner_bot.**"""
+        f"""❖ Hᴇʏ  {message.from_user.first_name} !, Nɪᴄᴇ Tᴏ Mᴇᴇᴛ Yᴏᴜ !\n\nYᴏᴜ Cᴀɴ Mᴀᴋᴇ Yᴏᴜʀ Oᴡɴ Rᴇᴀᴄᴛɪᴏɴ Bᴏᴛ Bʏ @ReactionCloner_bot.""",
+        reply_markup=buttons
     )
