026cacb19984a9769baa7781d862a2c5a8c25096
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-27 00:16:43 +0530

    Update Start.py

--- nexichat/mplugin/Start.py
+++ nexichat/mplugin/Start.py
@@ -5,20 +5,5 @@ from nexichat import nexichat as app
 @Client.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
     await message.reply_text(
-                   f"""**❖ нᴇʏ  {message.from_user.first_name} !, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
-━━━━━━━━━━━━━━━━━━━━━━━━━━━
-
-● ɪ ᴀᴍ  {(await client.get_me()).mention} !
-
-
-⦿━━━━━━━━━━━━━━━━━━━━━⦿
-❍ •  ɪ'ʟʟ ʀᴇᴀᴄᴛ ᴛᴏ ᴇᴠᴇʀʏ ᴍᴇssᴀɢᴇ  •
-│❍ • ʙᴇsᴛ ᴄʟᴏɴᴇ ғᴇᴀᴛᴜʀᴇ •
-│❍ • ɴᴏ ʟᴀɢs + ɴᴏ ᴀᴅs •
-│❍ • 24x7 ᴏɴʟɪɴᴇ sᴜᴘᴘᴏʀᴛ •
-│❍ • ᴀ ᴘᴏᴡᴇʀғᴜʟ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ •
-❍ • ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs •
-⦿━━━━━━━━━━━━━━━━━━━━━⦿
-
-❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •\n\n❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ @ReactionCloner_bot**"""
+                   f"""**❖ Hᴇʏ  {message.from_user.first_name} !, Nɪᴄᴇ Tᴏ Mᴇᴇᴛ Yᴏᴜ !\n\nYᴏᴜ Cᴀɴ Mᴀᴋᴇ Yᴏᴜʀ Oᴡɴ Rᴇᴀᴄᴛɪᴏɴ Bᴏᴛ Bʏ @ReactionCloner_bot.**"""
     )
