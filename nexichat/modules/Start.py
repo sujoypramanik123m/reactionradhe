4ba8846ff75a7d4b90bc64783f41521ec1df0f45
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-06-27 01:00:43 +0530

    Update Start.py

--- nexichat/modules/Start.py
+++ nexichat/modules/Start.py
@@ -1,11 +1,18 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from nexichat import nexichat as app
 
 @app.on_message(filters.command("start"))
 async def start_message(client: Client, message: Message):
+    buttons = InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/SuperToppers0'),
+            InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ɢʀᴏᴜᴘ', url='https://t.me/SuperToppers')
+        ]
+    ])
+
     await message.reply_text(
-                   f"""**❖ Hᴇʏ  {message.from_user.first_name}✨, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
+        f"""**❖ Hᴇʏ  {message.from_user.first_name}✨, ɴɪᴄᴇ ᴛᴏ ᴍᴇᴇᴛ ʏᴏᴜ !
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ● ɪ ᴀᴍ  {(await client.get_me()).mention} ⚡
 ⦿━━━━━━━━━━━━━━━━━━━━━⦿
@@ -17,5 +24,6 @@ async def start_message(client: Client, message: Message):
 ❍ • ɪ ʜᴀᴠᴇ sᴘᴇᴄɪᴀʟ ғᴇᴀᴛᴜʀᴇs •
 ⦿━━━━━━━━━━━━━━━━━━━━━⦿
 
-❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •\n\n❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ @ReactionCloner_bot**"""
+❖ ᴛʜɪs ɪs ᴘᴏᴡᴇʀғᴜʟ ʙᴏᴛ, ғᴏʀ ʏᴏᴜʀ ɢʀᴏᴜᴘ/ᴄʜᴀɴɴᴇʟ  •\n\n❍ • ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ʙᴏᴛ ʙʏ @ReactionCloner_bot**""",
+        reply_markup=buttons
     )
